0b3c8f12bd64aba6f4a5dc840653044f1262dd16
Shubham Gupta <shubhamedu.01@gmail.com>
2025-05-07 18:14:26 +0530

    push

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center">
         <div class="h-dvh  max-w-xl w-full">
-            <slot>Hello</slot>
+            <slot>SHUBHAM</slot>
         </div>
     </div>
 </template>
