3fca685de88ad8a08d28be24c3d53d8aad7f7918
Shubham Gupta <shubhamedu.01@gmail.com>
2025-05-12 14:49:50 +0530

    test: remove git config

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center">
         <div class="h-dvh  max-w-xl w-full">
-            <slot>SHUBHAM</slot>
+            <slot>SHUBHAM HELLO</slot>
         </div>
     </div>
 </template>
