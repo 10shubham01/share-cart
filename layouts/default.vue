53f8471c934636e2b3e398ae60c0a812ac3834a4
Shubham Gupta <shubhamedu.01@gmail.com>
2025-05-29 15:01:38 +0530

    test: new

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center">
         <div class="h-dvh  max-w-xl w-full">
-            <slot>by world</slot>
+            <slot>by world1</slot>
         </div>
     </div>
 </template>
